628bee8e30c210984c99884251ff6cf9e7f5d7ab
Daniel Pfeffer <mail@danielpfeffer.de>
2019-08-14 20:56:36 +0200

    added event routes

--- goPhoBo.go
+++ goPhoBo.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/looplab/fsm"
-
 	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/looplab/fsm"
 )
 
 // PhoBo structure, containing all needed stuff
 type PhoBo struct {
 	nameOfParty string
 	FSM         *fsm.FSM
+	cntPhotos   uint64
 }
 
 var pPhoBo *PhoBo
@@ -49,6 +47,8 @@ func NewPhoBo(newNameOfParty string) *PhoBo {
 		},
 	)
 
+	d.cntPhotos = 0
+
 	return d
 }
 
@@ -58,10 +58,11 @@ func (d *PhoBo) enterState(e *fsm.Event) {
 
 func (d *PhoBo) beforeEvent(e *fsm.Event) {
 	fmt.Printf("New event: \"%s\"\n", e.Event)
+
 }
 
 func (d *PhoBo) cbDoPhoto(e *fsm.Event) {
-
+	d.cntPhotos++
 }
 
 func (d *PhoBo) cbDeletePhoto(e *fsm.Event) {
@@ -80,47 +81,19 @@ func (d *PhoBo) cbEndSmile(e *fsm.Event) {
 
 }
 
-// spaHandler implements the http.Handler interface, so we can use it
-// to respond to HTTP requests. The path to the static directory and
-// path to the index file within that static directory are used to
-// serve the SPA in the given static directory.
-type spaHandler struct {
-	staticPath string
-	indexPath  string
+func (d *PhoBo) endSmileAfter(t time.Duration) {
+	time.Sleep(t)
+	d.FSM.Event("endSmile")
 }
 
-// ServeHTTP inspects the URL path to locate a file within the static dir
-// on the SPA handler. If a file is found, it will be served. If not, the
-// file located at the index path on the SPA handler will be served. This
-// is suitable behavior for serving an SPA (single page application).
-func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// get the absolute path to prevent directory traversal
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-		// if we failed to get the absolute path respond with a 400 bad request
-		// and stop
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+func (d *PhoBo) decideForMeAfter(t time.Duration) {
+	time.Sleep(t)
+	if rand.Float32() < 0.5 {
+		d.FSM.Event("deletePhoto")
+	} else {
+		d.FSM.Event("acceptPhoto")
 	}
 
-	// prepend the path with the path to the static directory
-	path = filepath.Join(h.staticPath, path)
-
-	// check whether a file exists at the given path
-	_, err = os.Stat(path)
-	if os.IsNotExist(err) {
-		// file does not exist, serve index.html
-		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
-		return
-	} else if err != nil {
-		// if we got an error (that wasn't that the file doesn't exist) stating the
-		// file, return a 500 internal server error and stop
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// otherwise, use http.FileServer to serve the static dir
-	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func main() {
@@ -129,19 +102,33 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
-		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	router.HandleFunc("/doPhoto", func(w http.ResponseWriter, r *http.Request) {
+		pPhoBo.FSM.Event("doPhoto")
+		go pPhoBo.decideForMeAfter(1 * time.Second)
+		json.NewEncoder(w).Encode(map[string]bool{"eventSuccess": true})
+		json.NewEncoder(w).Encode(map[string]uint64{"cntPhotos": pPhoBo.cntPhotos})
 	})
 
-	router.HandleFunc("/doPhoto", func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
-		pPhoBo.FSM.Event("doPhoto")
+	router.HandleFunc("/deletePhoto", func(w http.ResponseWriter, r *http.Request) {
+		pPhoBo.FSM.Event("deletePhoto")
 		json.NewEncoder(w).Encode(map[string]bool{"eventSuccess": true})
 	})
 
-	spa := spaHandler{staticPath: "build", indexPath: "index.html"}
-	router.PathPrefix("/").Handler(spa)
+	router.HandleFunc("/acceptPhoto", func(w http.ResponseWriter, r *http.Request) {
+		pPhoBo.FSM.Event("acceptPhoto")
+		json.NewEncoder(w).Encode(map[string]bool{"eventSuccess": true})
+	})
+
+	router.HandleFunc("/beginSmile", func(w http.ResponseWriter, r *http.Request) {
+		pPhoBo.FSM.Event("beginSmile")
+		go pPhoBo.endSmileAfter(3 * time.Second)
+		json.NewEncoder(w).Encode(map[string]bool{"eventSuccess": true})
+	})
+
+	router.HandleFunc("/endSmile", func(w http.ResponseWriter, r *http.Request) {
+		pPhoBo.FSM.Event("endSmile")
+		json.NewEncoder(w).Encode(map[string]bool{"eventSuccess": true})
+	})
 
 	srv := &http.Server{
 		Handler: router,
@@ -153,12 +140,12 @@ func main() {
 
 	log.Fatal(srv.ListenAndServe())
 
-	mPhoBo.FSM.Event("doPhoto")
-	mPhoBo.FSM.Event("acceptPhoto")
-	mPhoBo.FSM.Event("doPhoto")
-	mPhoBo.FSM.Event("deletePhoto")
-	mPhoBo.FSM.Event("beginSmile")
-	mPhoBo.FSM.Event("endSmile")
+	// mPhoBo.FSM.Event("doPhoto")
+	// mPhoBo.FSM.Event("acceptPhoto")
+	// mPhoBo.FSM.Event("doPhoto")
+	// mPhoBo.FSM.Event("deletePhoto")
+	// mPhoBo.FSM.Event("beginSmile")
+	// mPhoBo.FSM.Event("endSmile")
 
-	time.Sleep(3 * time.Second)
+	// time.Sleep(3 * time.Second)
 }
